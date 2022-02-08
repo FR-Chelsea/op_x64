193a3182924e6198ecc5f996c9fca72d5bb15b7f
FR-Chelsea <35002212+FR-Chelsea@users.noreply.github.com>
2022-02-09 00:03:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-#sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 
-git clone https://github.com/kenzok8/openwrt-packages package/kenzok8
-git clone https://github.com/ntlf9t/luci-app-dnspod  package/dnspod
-git clone https://github.com/liuran001/openwrt-packages package/liuran001
+
 
